828a5f1279d74c75c6e1d4d94167b81461cf61be
smithymmij <114240428+smithymmij@users.noreply.github.com>
2024-02-18 19:36:23 -0300

    adicionando header e footer

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -7,6 +7,17 @@ exports.homepage = async (req, res) => {
         title: "Notas",
         description: "Aplicativo de Notas.",
     }
-
     res.render('index', locals);
+}
+
+/**
+ * GET /
+ * About
+ */
+exports.about = async (req, res) => { 
+    const locals = {
+        title: "About - Notas",
+        description: "Aplicativo de Notas.",
+    }
+    res.render('about', locals);
 }
