728cb7e3fcbc5c13a65fff6bc949474a5ad8f017
smithymmij <114240428+smithymmij@users.noreply.github.com>
2024-02-21 23:42:49 -0300

    modificando pro render

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,8 +7,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    callbackURLF: process.env.GOOGLE_CALLBACK_URL_F
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
